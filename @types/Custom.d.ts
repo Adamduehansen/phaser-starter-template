60cb9cd12475017543827050bab2c12a576cae85
Adam Due Hansen <adamduehan@gmail.com>
2022-02-02 07:46:01 +0100

    Removed custom game object.

--- @types/Custom.d.ts
+++
@@ -1,7 +0,0 @@
-declare interface ICustom extends Phaser.Physics.Arcade.Sprite {}
-
-declare namespace Phaser.GameObjects {
-  interface GameObjectFactory {
-    custom(x: number, y: number): ICustom;
-  }
-}
